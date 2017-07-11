a6cd8f6d67e85e81a69d6f2fd392d3b960ea208b
Evgeniy Ivakha <ivahaev@gmail.com>
2017-07-11 11:50:39 +0500

    test updated

--- amigo_test.go
+++ amigo_test.go
@@ -8,44 +8,23 @@ func TestAmigo(t *testing.T) {
 	t.Run("#New", func(t *testing.T) {
 		t.Run("should return pointer to a new Amigo struct with username and password filled and default host and port settings", func(t *testing.T) {
 			a := New(&Settings{Username: "username", Password: "secret"})
-			if a.username != "username" {
+			if a.settings.Username != "username" {
 				t.Fatal("username mismatched")
 			}
-			if a.secret != "secret" {
+			if a.settings.Password != "secret" {
 				t.Fatal("secret mismatched")
 			}
 		})
 		t.Run("should return pointer to a new Amigo struct with username and password filled and provided host and default port settings", func(t *testing.T) {
 			a := New(&Settings{Username: "username", Password: "secret", Host: "amigo"})
-			if a.username != "username" {
+			if a.settings.Username != "username" {
 				t.Fatal("username mismatched")
 			}
-			if a.secret != "secret" {
+			if a.settings.Password != "secret" {
 				t.Fatal("secret mismatched")
 			}
-			if a.secret != "secret" {
-				t.Fatal("secret mismatched")
-			}
-			if a.host != "amigo" {
+			if a.settings.Host != "amigo" {
 				t.Fatal("host mismatched")
-			}
-		})
-		t.Run("should return pointer to a new Amigo struct with username, password, host and port filled", func(t *testing.T) {
-			a := New(&Settings{Username: "username", Password: "secret", Host: "amigo", Port: "666"})
-			if a.username != "username" {
-				t.Fatal("username mismatched")
-			}
-			if a.secret != "secret" {
-				t.Fatal("secret mismatched")
-			}
-			if a.secret != "secret" {
-				t.Fatal("secret mismatched")
-			}
-			if a.host != "amigo" {
-				t.Fatal("host mismatched")
-			}
-			if a.port != "666" {
-				t.Fatal("port mismatched")
 			}
 		})
 	})
